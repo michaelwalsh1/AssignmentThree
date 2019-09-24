eaa95d301cfec625104af9bd027f9d880ac6c5f3
DaveAvid <31331401+DaveAvid@users.noreply.github.com>
2019-09-24 12:10:08 -0500

    Add files via upload

---
+++ PurpleBoxAdminInterface.java
@@ -0,0 +1,28 @@
+package com.David.maven.PurpleBox;
+
+public interface PurpleBoxAdminInterface {
+	
+	public void addMovieToInventory()
+	
+	public void removeMovieFromInventory()
+	
+	public void setPriceDVD(DVD)
+	
+	public void setPriceBluRay(BluRay)
+	
+	public void setPriceAllGames()
+	
+	public void setVolumeDiscount()
+	
+	public void setPromoCodes()
+	
+	public void disableUnit()
+	
+	public void enableUnit()
+	
+	
+	
+	
+	
+
+}
---
+++ PurpleBoxUserInterface.java
@@ -0,0 +1,31 @@
+package com.David.maven.PurpleBox;
+
+public interface PurpleBoxUserInterface {
+	
+	public void removeFromCart(String name)
+	
+	public void addToCard(Product item)
+	
+	public void emptyCard()
+	
+	public void checkout()
+	
+	public void paymentMethod()
+	
+	public boolean isMovieAvailable()
+	
+	public void enterEmail()
+	
+	public void returnMovie()
+	
+	public void peekCart()
+	
+	public void searchMethod()
+	
+	public void sortMethod()
+	
+	
+	
+	
+
+}
