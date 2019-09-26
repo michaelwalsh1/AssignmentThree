fa9f9eb8303a809cc30b16cd140406b90cb747ae
DaveAvid <31331401+DaveAvid@users.noreply.github.com>
2019-09-26 11:21:07 -0500

    Add files via upload

--- PurpleBoxAdminInterface.java
+++ PurpleBoxAdminInterface.java
@@ -2,27 +2,156 @@ package com.David.maven.PurpleBox;
 
 public interface PurpleBoxAdminInterface {
 	
-	public void addProductToInventory()
-	
-	public void removeProductFromInventory()
-	
-	public void setPriceDVD(DVD)
-	
-	public void setPriceBluRay(BluRay)
-	
-	public void setPriceAllGames()
-	
-	public void setVolumeDiscount()
-	
-	public void setPromoCodes()
-	
-	public void disableUnit()
-	
-	public void enableUnit()
-	
-	
-	
-	
-	
+	/**
+	   * The Interface PurpleBoxUserInterface.
+	   */
+	   public interface PurpleBoxUserInterface {
 
-}
+	       /**
+	       * Adds the movie.
+	       *
+	       * @param movie the movie
+	       */
+	       public void addMovie(Movie movie);
+
+	       /**
+	       * Removes the movie.
+	       *
+	       * @param movie the movie
+	       */
+	       public void removeMovie(Movie movie);
+
+	       /**
+	       * Removes the all movie.
+	       */
+	       public void removeAllMovie();
+
+	       /**
+	       * Checks if is movie available.
+	       *
+	       * @param movie the movie
+	       * @return true, if is movie available
+	       */
+	       public boolean isMovieAvailable(Movie movie);
+
+	       /**
+	       * Checkout.
+	       *
+	       * @param 
+	       */
+	       public void checkout(String email);
+
+	       /**
+	       * Make payment.
+	       *
+	       * @param 
+	       */
+	       public void makePayment(String method);
+
+	       /**
+	       * Return movies.
+	       *
+	       * @return the array list
+	       */
+	       public ArrayList<Movie> returnMovies();
+
+	       /**
+	       * Apply promo code.
+	       *
+	       * @param code the code
+	       */
+	       public void applyPromoCode(String code);
+
+	       /**
+	       * Adds the game.
+	       *
+	       * @param game the game
+	       * @param type the type
+	       */
+	       public void addGame(Game game, String type);
+
+	       /**
+	       * Removes the game.
+	       *
+	       * @param game the game
+	       * @param type the type
+	       */
+	       public void removeGame(Game game, String type);
+
+	       /**
+	       * Removes the all game.
+	       */
+	       public void removeAllGame();
+
+	       /**
+	       * Checks if is game available.
+	       *
+	       * @param game the game
+	       * @param type the type
+	       * @return true, if is game available
+	       */
+	       public boolean isGameAvailable(Game game, String type);
+
+	       /**
+	       * Return games.
+	       *
+	       * @return the array list
+	       */
+	       public ArrayList<Game> returnGames();
+
+	       /**
+	       * Check user account.
+	       *
+	       * @return the array list
+	       */
+	       public ArrayList<Item> checkUserAccount();
+	      
+	       /**
+	       * Adds the movie to inventory.
+	       *
+	       * @param 
+	       */
+	       public void addMovieToInventory(Movie movie);
+	      
+	       /**
+	       * Removie movie to inventory.
+	       *
+	       * @param 
+	       */
+	       public void removieMovieToInventory(Movie movie);
+	      
+	       /**
+	       * Change price.
+	       *
+	       * @param 
+	       */
+	       public void changePrice(Item item);
+	      
+	       /**
+	       * Volume discount.
+	       *
+	       * @param 
+	       */
+	       public void volumeDiscount(double amount);
+	      
+	       /**
+	       * Adds the promo codes.
+	       *
+	       * @param 
+	       */
+	       public void addPromoCodes(ArrayList<String> codes);
+	      
+	       /**
+	       * Disable unit.
+	       *
+	       * @param item the item
+	       */
+	       public void disableUnit(Item item);
+	      
+	       /**
+	       * Enable unit.
+	       *
+	       * @param item the item
+	       */
+	       public void enableUnit(Item item);
+	}
--- PurpleBoxUserInterface.java
+++ PurpleBoxUserInterface.java
@@ -1,31 +1,103 @@
 package com.David.maven.PurpleBox;
 
 public interface PurpleBoxUserInterface {
-	
-	public void removeFromCart(String name)
-	
-	public void addToCard(Product item)
-	
-	public void emptyCard()
-	
-	public void checkout()
-	
-	public void paymentMethod()
-	
-	public boolean isMovieAvailable()
-	
-	public void enterEmail()
-	
-	public void returnMovie()
-	
-	public void peekCart()
-	
-	public void searchMethod()
-	
-	public void sortMethod()
-	
-	
-	
-	
 
-}
+	   /**
+	   * Adds the movie.
+	   *
+	   * @param 
+	   */
+	   public void addMovie(Movie movie);
+
+	   /**
+	   * Removes the movie.
+	   *
+	   * @param 
+	   */
+	   public void removeMovie(Movie movie);
+
+	   /**
+	   * Removes the all movie.
+	   */
+	   public void removeAllMovie();
+
+	   /**
+	   * Checks if is movie available.
+	   *
+	   * @param 
+	   * @return true, if is movie available
+	   */
+	   public boolean isMovieAvailable(Movie movie);
+
+	   /**
+	   * Checkout.
+	   *
+	   * @param 
+	   */
+	   public void checkout(String email);
+
+	   /**
+	   * Make payment.
+	   *
+	   * @param 
+	   */
+	   public void makePayment(String method);
+
+	   /**
+	   * Return movies.
+	   *
+	   * @return the array list
+	   */
+	   public ArrayList<Movie> returnMovies();
+
+	   /**
+	   * Apply promo code.
+	   *
+	   * @param apply the code
+	   */
+	   public void applyPromoCode(String code);
+
+	   /**
+	   * Adds the game.
+	   *
+	   * @param 
+	   * @param 
+	   */
+	   public void addGame(Game game, String type);
+
+	   /**
+	   * Removes the game.
+	   *
+	   * @param 
+	   * @param 
+	   */
+	   public void removeGame(Game game, String type);
+
+	   /**
+	   * Removes all games.
+	   */
+	   public void removeAllGame();
+
+	   /**
+	   * Checks if is game available.
+	   *
+	   * @param type of game
+	   * @return true, if is game available
+	   */
+	   public boolean isGameAvailable(Game game, String type);
+
+	   /**
+	   * Return games.
+	   *
+	   * @return the array list
+	   */
+	   public ArrayList<Game> returnGames();
+
+	   /**
+	   * Check user account.
+	   *
+	   * @return the array list
+	   */
+	   public ArrayList<Item> checkUserAccount();
+
+	}
