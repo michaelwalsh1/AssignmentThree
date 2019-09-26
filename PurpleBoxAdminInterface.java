26988a00254b952003832fdfccfa132a582a6b1f
Allie Wolf <55759277+alliekwolf@users.noreply.github.com>
2019-09-26 13:44:17 -0500

    Update PurpleBoxAdminInterface.java

--- PurpleBoxAdminInterface.java
+++ PurpleBoxAdminInterface.java
@@ -19,85 +19,20 @@ public interface PurpleBoxAdminInterface {
 	       *
 	       * @param 
 	       */
-	       public void removeMovie(Movie movie);
+	       public void removeProductFromInventory(Product someProduct);
 
 	       /**
 	       * This method allows the admin to remove all movies from the purplebox.
 	       */
-	       public void removeAllMovie();
+	       public void removeAllFromInventory();
 
-	       /**
-	       * This method checks if a movie is available.
-	       *
-	       * @param 
-	       * @return true, if movie is available
-	       */
-	       public boolean isMovieAvailable(Movie movie);
-
-	       /**
-	       * Apply promo code.
-	       *
-	       * @param 
-	       */
-	       public void applyPromoCode(String code);
-
-	       /**
-	       * Adds the game.
-	       *
-	       * @param 
-	       * @param 
-	       */
-	       public void addGame(Game game, String type);
-
-	       /**
-	       * Removes the game.
-	       *
-	       * @param 
-	       * @param 
-	       */
-	       public void removeGame(Game game, String type);
-
-	       /**
-	       * Removes the all game.
-	       */
-	       public void removeAllGame();
-
-	       /**
-	       * Checks if is game available.
-	       *
-	       * @param 
-	       * @param 
-	       * @return true, if is game available
-	       */
-	       public boolean isGameAvailable(Game game, String type);
-
-	       /**
-	       * Return games.
-	       *
-	       * @return the array list
-	       */
-	       public ArrayList<Game> returnGames();
 		   
-	       /**
-	       * Adds the movie to inventory.
-	       *
-	       * @param 
-	       */
-	       public void addMovieToInventory(Movie movie);
-	      
-	       /**
-	       * Remove movie to inventory.
-	       *
-	       * @param 
-	       */
-	       public void removieMovieToInventory(Movie movie);
-	      
 	       /**
 	       * This method allows thse admin to change the price of any product.
 	       *
 	       * @param item
 	       */
-	       public void changePrice(Item item);
+	       public void changePrice(Product someProduct);
 	      
 	       /**
 	       * This method allows the admin to make changes to the volume discount.
@@ -111,7 +46,7 @@ public interface PurpleBoxAdminInterface {
 	       *
 	       * @param 
 	       */
-	       public void addPromoCodes(ArrayList<String> codes);
+	       public void setPromoCodes(ArrayList<String> codes);
 	      
 	       /**
 	       * This method allows the admin to disable the entire unit.
