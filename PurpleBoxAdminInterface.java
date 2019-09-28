6e9a432081018d55c21915a4a54da0868b00b21e
David Wagner <31331401+DaveAvid@users.noreply.github.com>
2019-09-28 13:40:52 -0500

    Update PurpleBoxAdminInterface.java

--- PurpleBoxAdminInterface.java
+++ PurpleBoxAdminInterface.java
@@ -1,28 +1,64 @@
 package com.David.maven.PurpleBox;
 
-public interface PurpleBoxAdminInterface {
-	
-	public void addProductToInventory()
-	
-	public void removeProductFromInventory()
-	
-	public void setPriceDVD(DVD)
-	
-	public void setPriceBluRay(BluRay)
-	
-	public void setPriceAllGames()
-	
-	public void setVolumeDiscount()
-	
-	public void setPromoCodes()
-	
-	public void disableUnit()
-	
-	public void enableUnit()
-	
-	
-	
-	
-	
+import java.util.ArrayList;
 
+public interface PurpleBoxAdminInterface {
+	/**
+	* This method allows the admin to add a product to the purplebox.
+	*
+	* @param Product to be added to ArrayList<Product>.
+	*/
+	public void addProductToInventory(Product someProduct);
+	
+	/**
+	* This method allows the admin to remove a product from the purplebox.
+	*
+	* @param Product to be removed from inventory ArrayList<Product>.
+	* @returns true if product was removed.
+	*/
+	public boolean removeProductFromInventory(Product someProduct);
+	
+	/**
+	* This method allows the admin to remove all Products from the purplebox.
+	* 
+	* @ensure all Products removed from ArrayList<Product>.
+	*/
+	public void removeAllFromInventory();
+
+	
+	/**
+	* This method allows the admin to change the price of any Product.
+	*
+	* @param ArrayList<Product> of all Products.
+	* @param Game price to be changed.
+	*/
+	public void changePrice(Product product, double newPrice);
+	
+	/**
+	* This method allows the admin to make set the volume discount.
+	*
+	* @param double discount is percentage (ex. 0.5 for 50%) of discount.
+	*/
+	public void setVolumeDiscount(double discount);
+	
+	/**
+	* Adds the promo codes.
+	*
+	* @param ArrayList<String> of promo codes.
+	*/
+	public void setPromoCodes(ArrayList<String> codes);
+	
+	/**
+	* This method allows the admin to disable the entire unit.
+	*
+	* @param 
+	*/
+	public void disableUnit();
+	
+	/**
+	* This method allows the admin to enable the entire unit.
+	*
+	* @param 
+	*/
+	public void enableUnit();
 }
