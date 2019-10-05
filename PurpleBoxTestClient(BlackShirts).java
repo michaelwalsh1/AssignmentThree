29ce98db9da1664f22a10b9ff49c931ac981cb88
Allie Wolf <55759277+alliekwolf@users.noreply.github.com>
2019-10-04 22:18:33 -0500

    Update PurpleBoxTestClient(BlackShirts).java

--- PurpleBoxTestClient(BlackShirts).java
+++ PurpleBoxTestClient(BlackShirts).java
@@ -11,8 +11,10 @@ public class PurpleBoxTestMain {
 		
 		//variables
 		int userInput;
-		ArrayList<Product> allProducts = new ArrayList<Product>();
+		PurpleBox purpleBox = new PurpleBox();
+		ArrayList<Product> allProducts = createProductList();
 		ArrayList<Product> cart = new ArrayList<Product>();
+		ArrayList<Product> movieList = createMovieList(allProducts);
 		
 		//call printMainMenu() method
 		printMainMenu();
@@ -24,7 +26,8 @@ public class PurpleBoxTestMain {
 			case 1:
 				userInput = -1;   //reset userInput
 				ArrayList<Product> allMovies = new ArrayList<Product>();
-				allMovies = PurpleBox.viewAllMovies(allProducts);
+				//allMovies = purpleBox.viewAllMovies(allProducts);
+				allMovies = movieList;
 				
 				printMovieMenu();
 				
@@ -34,14 +37,14 @@ public class PurpleBoxTestMain {
 				switch(userInput) {   //movie menu cases
 					case 1:   //view all movies
 						userInput = -1;
-						makeSelectionMenu(allMovies, cart);
+						makeSelectionMenu(allMovies, cart, purpleBox);
 						break;
 					case 2:   //search by name
 						userInput = -1;
 						break;
 					case 3:   //search by genre
 						userInput = -1;
-						String genre;
+						String genre = "";
 						ArrayList<Product> listByGenre = new ArrayList<Product>();
 						
 						System.out.println("Which genre are you looking for?\n"
@@ -85,23 +88,23 @@ public class PurpleBoxTestMain {
 						userInput = -1;
 						
 						//call PurpleBox's searchByGenre() method
-						listByGenre = PurpleBox.searchByGenre(allMovies, genre);
+						listByGenre = purpleBox.searchByGenre(allMovies, genre);
 						
-						makeSelectionMenu(listByGenre, cart);
+						makeSelectionMenu(listByGenre, cart, purpleBox);
 						
 						break;
 					case 4:   //search by disc type
 						userInput = -1;
-						String format;
+						String format = "";
 						ArrayList<Product> listByFormat = new ArrayList<Product>();
 						
 						System.out.println("Which disc format are you looking for?\n"
 								+ "  1 -- DVD\n"
 								+ "  2 -- Blu-Ray\n");
 						System.out.print("Choose a number 1-2: ");
-						int inputM = console.nextInt();
+						userInput = console.nextInt();
 						
-						switch(inputM) {
+						switch(userInput) {
 							case 1:
 								format = "DVD";
 								break;
@@ -112,9 +115,9 @@ public class PurpleBoxTestMain {
 						userInput = -1;
 						
 						//call PurpleBox's searchByGenre() method
-						listByFormat = PurpleBox.searchByFormat(allMovies, format);
+						listByFormat = purpleBox.searchByFormat(allMovies, format);
 						
-						makeSelectionMenu(listByFormat, cart);
+						makeSelectionMenu(listByFormat, cart, purpleBox);
 						
 						break;
 					case 5:   //search by release date
@@ -131,7 +134,7 @@ public class PurpleBoxTestMain {
 			case 2:
 				userInput = -1;   //reset userInput
 				ArrayList<Product> allGames = new ArrayList<Product>();
-				allGames = PurpleBox.viewAllGames(allProducts);
+				allGames = purpleBox.viewAllGames(allProducts);
 				
 				printGameMenu();
 				
@@ -141,7 +144,7 @@ public class PurpleBoxTestMain {
 				switch(userInput) {   //game menu cases
 					case 1:   //view all games
 						userInput = -1;
-						makeSelectionMenu(allGames, cart);
+						makeSelectionMenu(allGames, cart, purpleBox);
 						break;
 					case 2:   //search by name
 						userInput = -1;
@@ -217,17 +220,17 @@ public class PurpleBoxTestMain {
 				+ "\n");
 	}
 	
-	public static void makeSelectionMenu(ArrayList<Product> productList, ArrayList<Product> cart) {
+	public static void makeSelectionMenu(ArrayList<Product> productList, ArrayList<Product> cart, PurpleBox purpleBox) {
 		
 		boolean flag = false;
 		char userInput;
 		String someTitle;
-		Product foundProduct = null;
+		Product foundProduct = productList.get(5);
 		
 		while(true) {
 			
 			//print list of products
-			System.out.println(productList);
+			System.out.println("\n" + productList);
 			
 			System.out.print("\nDid you find what you were looking for?\n"
 					+ "Type 'Y' for 'Yes', 'N' for 'No': ");
@@ -236,11 +239,11 @@ public class PurpleBoxTestMain {
 			if(userInput == 'Y') {
 				while(true) {
 					System.out.print("\n\nOkay! Type in a title, then press Enter: ");
-					someTitle = console.nextLine();
+					someTitle = console.next();
 					
 					//search productList for someTitle
 					for(int i = 0; i < productList.size() - 1; i++) {
-						if(someTitle.compareTo(productList.get(i).getName()) == 0) {
+						if(productList.get(i).getName().contains(someTitle)) {
 							foundProduct = productList.get(i);
 							System.out.println("\n\nFound it! Add to cart?"
 									+ "Type 'Y' for 'Yes', 'N' for 'No': ");
@@ -248,7 +251,7 @@ public class PurpleBoxTestMain {
 							
 							if(userInput == 'Y') {
 								//call PurpleBox's addToCart() method
-								PurpleBox.addToCart(cart, foundProduct);
+								purpleBox.addToCart(cart, foundProduct);
 							}
 							break;
 						}
@@ -284,6 +287,48 @@ public class PurpleBoxTestMain {
 		
 		
 	}
+	
+	public static ArrayList<Product> createProductList(){
+		
+		ArrayList<Product> productList = new ArrayList<Product>();
+		
+		Product movie1 = new Product("MovieA", "Family", "DVD", "2018", 5, 2.00, true);
+		Product movie2 = new Product("MovieB", "Drama", "DVD", "2018", 3, 2.00, true);
+		Product movie3 = new Product("MovieC", "Action", "DVD", "2019", 4, 2.00, true);
+		Product movie4 = new Product("MovieD", "Action", "Blu-Ray", "2019", 4, 2.50, true);
+		Product movie5 = new Product("MovieE", "Family", "Blu-Ray", "2018", 3, 2.50, true);
+		Product movie6 = new Product("MovieF", "Sci-Fi", "Blu-Ray", "2019", 5, 2.50, true);
+		Product game1 = new Product("GameA", "Action", "PS4", "2018", 5, 1.50, true);
+		Product game2 = new Product("GameB", "Action", "PS4", "2019", 5, 1.50, true);
+		Product game3 = new Product("GameC", "Action", "XBox", "2019", 5, 1.50, true);
+		Product game4 = new Product("GameD", "Action", "XBox", "2018", 5, 1.50, true);
+		
+		productList.add(movie1);
+		productList.add(movie2);
+		productList.add(movie3);
+		productList.add(movie4);
+		productList.add(movie5);
+		productList.add(movie6);
+		productList.add(game1);
+		productList.add(game2);
+		productList.add(game3);
+		productList.add(game4);
+		
+		
+		return productList;
+	}
+	
+	public static ArrayList<Product> createMovieList(ArrayList<Product> productList) {
+		
+		ArrayList<Product> movieList = new ArrayList<Product>();
+		
+		for(int i = 0; i < productList.size() - 1; i++) {
+			if(productList.get(i).getMediaType().equals("DVD") || productList.get(i).getMediaType().equals("Blu-Ray")) {
+				movieList.add(productList.get(i));
+			}
+		}
+		
+		return movieList;
+	}
+	
 }
-
-
